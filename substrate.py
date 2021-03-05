3cb673b9c205e211c1686f7f05501fa8f0392b27
Ralph Loader <ralph1loader@gmail.com>
2021-03-06 04:49:39 +1300

    Change the PMOS switch

--- substrate.py
+++ substrate.py
@@ -9,8 +9,8 @@ TRANS = None
 for L in FILE:
     L = L.strip()
     words = L.split()
-    if words and words[0].startswith('M') and words[4].startswith('PMOS'):
-        words.insert(4, words[3])
+#    if words and words[0].startswith('M') and words[4].startswith('PMOS'):
+#        words.insert(4, words[3])
     if words and words[0] == 'tran':
         TRANS = L
         continue

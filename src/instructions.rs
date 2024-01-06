ee580cb475702543826f5d06ba44183b72dcafa0
Ralph Loader <ralph1loader@gmail.com>
2024-01-07 09:24:13 +1300

    Get rid of the trait.

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,5 +1,4 @@
 
-use ArithOp::*;
 use Condition::*;
 use Instruction::*;
 use Register::*;
@@ -25,8 +24,14 @@ pub enum Target {
 
 type Labels = std::collections::HashMap<String, u8>;
 
-pub enum ArithOp { Add = 0x80, Sub = 0x84, Or  = 0x88, And = 0x8c,
-                   Adc = 0x90, Sbc = 0x94, Xor = 0x98, And2 = 0x9c }
+const ADD: u8 = 0x80;
+const SUB: u8 = 0x84;
+const OR : u8 = 0x88;
+const AND: u8 = 0x8c;
+const ADC: u8 = 0x90;
+const SBC: u8 = 0x94;
+const XOR: u8 = 0x98;
+//const AND2:u8 = 0x9c;
 
 pub enum XferOp {Inc = 0xc0, Dec = 0xc4, Mov = 0xc8, MovM = 0xcc}
 
@@ -55,11 +60,11 @@ pub struct Instructions {
     pub labels: Labels,
 }
 
-impl From<u8> for Value { fn from(v: u8) -> Value { Number(v) } }
+impl From<u8> for Value { fn from(v: u8) -> Value {Number(v)} }
 
-impl From<Register> for Value { fn from(r: Register) -> Value { Reg(r) } }
+impl From<Register> for Value { fn from(r: Register) -> Value {Reg(r)} }
 
-impl From<[u8; 1]> for Value { fn from([n]: [u8; 1]) -> Value { MemNumber(n) } }
+impl From<[u8; 1]> for Value { fn from([n]: [u8; 1]) -> Value {MemNumber(n)} }
 
 impl From<[Register; 1]> for Value {
     fn from([r]: [Register; 1]) -> Value { MemReg(r) }
@@ -75,99 +80,85 @@ impl From<&str> for Target {
     fn from(l: &str) -> Target { Target::Label(l.to_string()) }
 }
 
-impl Target {
-    fn resolve(&self, labels: &Labels) -> u8 {
-        match self {
-            Target::Addr(a) => *a,
-            Target::Label(l) => labels[l]
-        }
-    }
-}
-
 impl Instructions {
     pub fn assemble(&self) -> Vec<u8> {
         let mut r = Vec::default();
         let mut address: u8 = 0;
         for insn in &self.insns {
-            match insn {
-                Address(target) => {
-                    let a = target.resolve(&self.labels);
-                    r.push(a & 0x3f);
-                    address = a >> 6;
+            let (b, a) = match insn {
+                Address(Target::Addr(a)) => (a & 0x3f, a >> 6),
+                Address(Target::Label(l)) => {
+                    let a = self.labels[l];
+                    (a & 0x3f, a >> 6)
                 }
-                Byte(b) => {
-                    r.push(b + address);
-                    address = 0;
-                }
-            }
+                Byte(b) => (b + address, 0),
+            };
+            address = a;
+            r.push(b);
         }
         r
     }
-}
 
-impl AbstractAsm for &mut Instructions {
-    fn insn(self, insn: Instruction) -> Self {
+    fn insn(&mut self, insn: Instruction) -> &mut Self {
         self.insns.push(insn);
         self
     }
-    fn label(self, l: impl Into<String>) -> Self {
+    pub fn label(&mut self, l: impl Into<String>) -> &mut Self {
         self.labels.insert(l.into(), self.insns.len().try_into().unwrap());
         self
     }
-}
 
-pub trait AbstractAsm : Sized {
-    fn insn(self, insn: Instruction) -> Self;
-    fn label(self, l: impl Into<String>) -> Self;
-
-    fn byte(self, b: u8) -> Self { self.insn(Byte(b)) }
-    fn code(self, b: u8, v: impl Into<Value>) -> Self {
+    fn byte(&mut self, b: u8) -> &mut Self { self.insn(Byte(b)) }
+    fn code(&mut self, b: u8, v: impl Into<Value>) -> &mut Self {
         match v.into() {
             Reg(r) => self.byte(b + r as u8),
             Number(n) => self.byte(n & 0x3f).byte(b + (n >> 6)),
             MemReg(r) => self.byte(MEM + r as u8).byte(b),
-            MemNumber(n) => self.byte(n & 0x3f)
-                .insn(Byte(MEM + (n >> 6))).byte(b),
+            MemNumber(n) => self.byte(n & 0x3f).byte(MEM + (n >> 6)).byte(b),
         }
     }
 
-    fn mem(self, v: impl Into<Value>) -> Self { self.code(MEM, v) }
-    fn add(self, v: impl Into<Value>) -> Self { self.code(Add as u8, v) }
-    fn add(self, v: impl Into<Value>) -> Self { self.code(Add as u8, v) }
-    fn sub(self, v: impl Into<Value>) -> Self { self.code(Sub as u8, v) }
-    fn and(self, v: impl Into<Value>) -> Self { self.code(And as u8, v) }
-    fn or (self, v: impl Into<Value>) -> Self { self.code(Or  as u8, v) }
-    fn adc(self, v: impl Into<Value>) -> Self { self.code(Adc as u8, v) }
-    fn sbc(self, v: impl Into<Value>) -> Self { self.code(Sbc as u8, v) }
-    fn cmp(self, v: impl Into<Value>) -> Self { self.code(CMP, v) }
-    fn tst(self, v: impl Into<Value>) -> Self { self.code(TST, v) }
+    pub fn mem(&mut self, v: impl Into<Value>) -> &mut Self {self.code(MEM, v)}
+    pub fn add(&mut self, v: impl Into<Value>) -> &mut Self {self.code(ADD, v)}
+    pub fn sub(&mut self, v: impl Into<Value>) -> &mut Self {self.code(SUB, v)}
+    pub fn and(&mut self, v: impl Into<Value>) -> &mut Self {self.code(AND, v)}
+    pub fn or (&mut self, v: impl Into<Value>) -> &mut Self {self.code(OR , v)}
+    pub fn adc(&mut self, v: impl Into<Value>) -> &mut Self {self.code(ADC, v)}
+    pub fn sbc(&mut self, v: impl Into<Value>) -> &mut Self {self.code(SBC, v)}
+    pub fn xor(&mut self, v: impl Into<Value>) -> &mut Self {self.code(XOR, v)}
+    pub fn cmp(&mut self, v: impl Into<Value>) -> &mut Self {self.code(CMP, v)}
+    pub fn tst(&mut self, v: impl Into<Value>) -> &mut Self {self.code(TST, v)}
 
-    fn jp(self, cc: Condition, t: impl Into<Target>) -> Self {
+    pub fn jp(&mut self, cc: Condition, t: impl Into<Target>) -> &mut Self {
         self.insn(Address(t.into())).byte(JUMP + cc as u8)
     }
-    fn jump(self, t: impl Into<Target>) -> Self { self.jp(Always, t) }
+    pub fn jump(&mut self, t: impl Into<Target>) -> &mut Self {
+        self.jp(Always, t)
+    }
 
-    fn cl(self, cc: Condition, t: impl Into<Target>) -> Self {
+    pub fn cl(&mut self, cc: Condition, t: impl Into<Target>) -> &mut Self {
         self.insn(Address(t.into())).byte(CALL + cc as u8)
     }
-    fn call(self, t: impl Into<Target>) -> Self { self.cl(Always, t) }
+    pub fn call(&mut self, t: impl Into<Target>) -> &mut Self {
+        self.cl(Always, t)
+    }
 
-    fn rt(self, cc: Condition) -> Self { self.byte(RET + cc as u8) }
-    fn ret(self) -> Self { self.rt(Always) }
+    pub fn rt(&mut self, cc: Condition) -> &mut Self {self.byte(RET + cc as u8)}
+    pub fn ret(&mut self) -> &mut Self { self.rt(Always) }
 
-    fn sta(self, v: impl Into<Value>) -> Self { self.code(STA, v) }
+    pub fn sta(&mut self, v: impl Into<Value>) -> &mut Self {self.code(STA, v)}
 
-    fn inc(self, r: Register) -> Self { self.incv(r, r) }
-    fn dec(self, r: Register) -> Self { self.decv(r, r) }
+    pub fn inc(&mut self, r: Register) -> &mut Self { self.incv(r, r) }
+    pub fn dec(&mut self, r: Register) -> &mut Self { self.decv(r, r) }
 
-    fn incv(self, r: Register, v: impl Into<Value>) -> Self {
+    pub fn incv(&mut self, r: Register, v: impl Into<Value>) -> &mut Self {
         self.code(Inc as u8 + r as u8 * 16, v)
     }
-    fn decv(self, r: Register, v: impl Into<Value>) -> Self {
+    pub fn decv(&mut self, r: Register, v: impl Into<Value>) -> &mut Self {
         self.code(Dec as u8 + r as u8 * 16, v)
     }
 
-    fn mov(self, d: Register, v: impl Into<Value>) -> Self {
+    pub fn mov(&mut self, d: Register, v: impl Into<Value>) -> &mut Self {
         match v.into() {
             Reg(r) => self.code(Mov as u8 + d as u8 * 16, r),
             Number(n) => self.code(Mov as u8 + d as u8 * 16, n),

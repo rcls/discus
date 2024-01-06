d11ca2ef7f18a24782c31d2b67552ca5bfdb18f7
Ralph Loader <ralph1loader@gmail.com>
2024-01-07 09:07:42 +1300

    Simplify.

--- src/instructions.rs
+++ src/instructions.rs
@@ -25,26 +25,28 @@ pub enum Target {
 
 type Labels = std::collections::HashMap<String, u8>;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum ArithOp { Add, Sub, Or, And, Adc, Sbc, Xor, And2 }
+pub enum ArithOp { Add = 0x80, Sub = 0x84, Or  = 0x88, And = 0x8c,
+                   Adc = 0x90, Sbc = 0x94, Xor = 0x98, And2 = 0x9c }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum XferOp { Inc, Dec, Mov, MovM }
+pub enum XferOp {Inc = 0xc0, Dec = 0xc4, Mov = 0xc8, MovM = 0xcc}
+
+pub enum Condition {
+    Always = 0, Never = 4, Always2 = 8, Never2 = 12,
+    Z = 16, NZ = 20, C = 24, NC = 28
+}
+
+const MEM: u8 = 0x5c;
+const CMP: u8 = 0x44;
+const TST: u8 = 0x6c;
+const STA: u8 = 0x4c;
+const RET: u8 = 0x60;
+const JUMP: u8 = 0;
+const CALL: u8 = 32;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Instruction {
+    Byte(u8),
     Address(Target),
-    // A 'Jump' must be preceded by 'Address'
-    Jump{call: bool, cc: Condition, target: Target},
-    Ret(Condition),
-    Const(u8),
-    Mem(Register),
-    Arith(ArithOp, Register),
-    ArithY(ArithOp, Register),          // Obscure!
-    Xfer(XferOp, Register, Register),
-    Sta(Register),
-    Cmp(Register),
-    Tst(Register),
 }
 
 #[derive(Clone, Debug, Default)]
@@ -73,20 +75,6 @@ impl From<&str> for Target {
     fn from(l: &str) -> Target { Target::Label(l.to_string()) }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum Condition {
-    Always, Never, Always2, Never2, Z, NZ, C, NC
-}
-
-fn registrate(n: u8) -> Register {
-    match n {
-        0x00..=0x3f => A,
-        0x40..=0x7f => X,
-        0x80..=0xbf => Y,
-        0xc0..=0xff => U,
-    }
-}
-
 impl Target {
     fn resolve(&self, labels: &Labels) -> u8 {
         match self {
@@ -96,36 +84,24 @@ impl Target {
     }
 }
 
-impl Instruction {
-    pub fn opcode(&self, labels: &Labels) -> u8 {
-        match self {
-            Address(target) => target.resolve(labels) & 0x3f,
-            Jump{call, cc, target} =>
-                *cc as u8 * 4 + (target.resolve(labels) >> 6)
-                + if *call { 0x40 } else { 0 },
-            &Arith (op, r) => 0x80 + op as u8 * 4 + r as u8,
-            &ArithY(op, r) => 0xa0 + op as u8 * 4 + r as u8,
-            &Xfer(op, d, r) => 0xc0 + op as u8 * 4 + d as u8 * 16 + r as u8,
-            Const(c) => c & 0x3f,
-            Mem(r) => 0x5c + *r as u8,
-            &Ret(cc) => 0x60 + cc as u8 * 4,
-            Sta(r) => 0x4c + *r as u8,
-            Cmp(r) => 0x44 + *r as u8,
-            Tst(r) => 0x6c + *r as u8,
-        }
-    }
-}
-
 impl Instructions {
-    pub fn program(&self) -> [u8; 256] {
-        let mut r = [0; 256];
-        for (insn, b) in self.assemble().zip(r.iter_mut()) {
-            *b = insn;
+    pub fn assemble(&self) -> Vec<u8> {
+        let mut r = Vec::default();
+        let mut address: u8 = 0;
+        for insn in &self.insns {
+            match insn {
+                Address(target) => {
+                    let a = target.resolve(&self.labels);
+                    r.push(a & 0x3f);
+                    address = a >> 6;
+                }
+                Byte(b) => {
+                    r.push(b + address);
+                    address = 0;
+                }
+            }
         }
         r
-    }
-    pub fn assemble(&self) -> impl Iterator<Item=u8> + '_ {
-        self.insns.iter().map(|insn| insn.opcode(&self.labels))
     }
 }
 
@@ -144,68 +120,59 @@ pub trait AbstractAsm : Sized {
     fn insn(self, insn: Instruction) -> Self;
     fn label(self, l: impl Into<String>) -> Self;
 
-    fn insn_value(self,
-                  i: impl FnOnce(Register) -> Instruction,
-                  v: impl Into<Value>) -> Self {
+    fn byte(self, b: u8) -> Self { self.insn(Byte(b)) }
+    fn code(self, b: u8, v: impl Into<Value>) -> Self {
         match v.into() {
-            Reg(r) => self.insn(i(r)),
-            Number(n) => self.insn(Const(n & 0x3f)).insn(i(registrate(n))),
-            MemReg(r) => self.insn(Mem(r)).insn(i(A)),
-            MemNumber(n) => self.insn(Const(n & 0x3f))
-                .insn(Mem(registrate(n))).insn(i(A)),
+            Reg(r) => self.byte(b + r as u8),
+            Number(n) => self.byte(n & 0x3f).byte(b + (n >> 6)),
+            MemReg(r) => self.byte(MEM + r as u8).byte(b),
+            MemNumber(n) => self.byte(n & 0x3f)
+                .insn(Byte(MEM + (n >> 6))).byte(b),
         }
     }
-    fn insn_arith(self, op: ArithOp, v: impl Into<Value>) -> Self {
-        self.insn_value(|r| Arith(op, r), v)
-    }
-    fn insn_xfer(self, op: XferOp, d: Register, v: impl Into<Value>) -> Self {
-        self.insn_value(|r| Xfer(op, d, r), v)
-    }
-    fn insn_jump(self, call: bool, cc: Condition, target: Target) -> Self {
-        self.insn(Address(target.clone())).insn(Jump{call, cc, target})
-    }
 
-    fn mem(self, v: impl Into<Value>) -> Self { self.insn_value(Mem, v) }
-    fn add(self, v: impl Into<Value>) -> Self { self.insn_arith(Add, v) }
-    fn sub(self, v: impl Into<Value>) -> Self { self.insn_arith(Sub, v) }
-    fn and(self, v: impl Into<Value>) -> Self { self.insn_arith(And, v) }
-    fn or (self, v: impl Into<Value>) -> Self { self.insn_arith(Or , v) }
-    fn adc(self, v: impl Into<Value>) -> Self { self.insn_arith(Adc, v) }
-    fn sbc(self, v: impl Into<Value>) -> Self { self.insn_arith(Sbc, v) }
-    fn cmp(self, v: impl Into<Value>) -> Self { self.insn_value(Cmp, v) }
-    fn tst(self, v: impl Into<Value>) -> Self { self.insn_value(Tst, v) }
+    fn mem(self, v: impl Into<Value>) -> Self { self.code(MEM, v) }
+    fn add(self, v: impl Into<Value>) -> Self { self.code(Add as u8, v) }
+    fn add(self, v: impl Into<Value>) -> Self { self.code(Add as u8, v) }
+    fn sub(self, v: impl Into<Value>) -> Self { self.code(Sub as u8, v) }
+    fn and(self, v: impl Into<Value>) -> Self { self.code(And as u8, v) }
+    fn or (self, v: impl Into<Value>) -> Self { self.code(Or  as u8, v) }
+    fn adc(self, v: impl Into<Value>) -> Self { self.code(Adc as u8, v) }
+    fn sbc(self, v: impl Into<Value>) -> Self { self.code(Sbc as u8, v) }
+    fn cmp(self, v: impl Into<Value>) -> Self { self.code(CMP, v) }
+    fn tst(self, v: impl Into<Value>) -> Self { self.code(TST, v) }
 
     fn jp(self, cc: Condition, t: impl Into<Target>) -> Self {
-        self.insn_jump(false, cc, t.into())
+        self.insn(Address(t.into())).byte(JUMP + cc as u8)
     }
     fn jump(self, t: impl Into<Target>) -> Self { self.jp(Always, t) }
 
     fn cl(self, cc: Condition, t: impl Into<Target>) -> Self {
-        self.insn_jump(true, cc, t.into())
+        self.insn(Address(t.into())).byte(CALL + cc as u8)
     }
     fn call(self, t: impl Into<Target>) -> Self { self.cl(Always, t) }
 
-    fn rt(self, cc: Condition) -> Self { self.insn(Ret(cc)) }
+    fn rt(self, cc: Condition) -> Self { self.byte(RET + cc as u8) }
     fn ret(self) -> Self { self.rt(Always) }
 
-    fn sta(self, v: impl Into<Value>) -> Self { self.insn_value(Sta, v) }
+    fn sta(self, v: impl Into<Value>) -> Self { self.code(STA, v) }
 
-    fn inc(self, r: Register) -> Self { self.insn_xfer(Inc, r, r) }
-    fn dec(self, r: Register) -> Self { self.insn_xfer(Dec, r, r) }
+    fn inc(self, r: Register) -> Self { self.incv(r, r) }
+    fn dec(self, r: Register) -> Self { self.decv(r, r) }
 
-    fn inc2(self, r: Register, v: impl Into<Value>) -> Self {
-        self.insn_xfer(Inc, r, v)
+    fn incv(self, r: Register, v: impl Into<Value>) -> Self {
+        self.code(Inc as u8 + r as u8 * 16, v)
     }
-    fn dec2(self, r: Register, v: impl Into<Value>) -> Self {
-        self.insn_xfer(Dec, r, v)
+    fn decv(self, r: Register, v: impl Into<Value>) -> Self {
+        self.code(Dec as u8 + r as u8 * 16, v)
     }
 
     fn mov(self, d: Register, v: impl Into<Value>) -> Self {
         match v.into() {
-            Reg(r) => self.insn_xfer(Mov, d, r),
-            Number(n) => self.insn_xfer(Mov, d, n),
-            MemReg(r) => self.insn_xfer(MovM, d, r),
-            MemNumber(n) => self.insn_xfer(MovM, d, n),
+            Reg(r) => self.code(Mov as u8 + d as u8 * 16, r),
+            Number(n) => self.code(Mov as u8 + d as u8 * 16, n),
+            MemReg(r) => self.code(MovM as u8 + d as u8 * 16, r),
+            MemNumber(n) => self.code(MovM as u8 + d as u8 * 16, n),
         }
     }
 }
@@ -216,16 +183,8 @@ fn test_basic() {
     i.add(24).sub(Y).adc([0x9b]).sbc([U]);
     println!("{:?}", i);
     assert_eq!(i.insns.len(), 8);
-    assert_eq!(i.insns[0], Const(24));
-    assert_eq!(i.insns[1], Arith(Add,A));
-    assert_eq!(i.insns[2], Arith(Sub,Y));
-    assert_eq!(i.insns[3], Const(0x1b));
-    assert_eq!(i.insns[4], Mem(Y));
-    assert_eq!(i.insns[5], Arith(Adc,A));
-    assert_eq!(i.insns[6], Mem(U));
-    assert_eq!(i.insns[7], Arith(Sbc,A));
 
-    let c: Vec<u8> = i.assemble().collect();
+    let c = i.assemble();
     assert_eq!(c.len(), 8);
     assert_eq!(c[0], 24);
     assert_eq!(c[1], 0x80);
@@ -247,13 +206,10 @@ fn test_parse_jump() {
     assert_eq!(i.labels.len(), 1);
     assert_eq!(i.labels["foo"], 2);
     assert_eq!(i.insns[0], Address(Target::Label("foo".into())));
-    assert_eq!(i.insns[1], Jump{call: true, cc: Always,
-                                target: Target::Label("foo".into())});
-    assert_eq!(i.insns[2], Ret(Always));
 
-    let c: Vec<u8> = i.assemble().collect();
+    let c = i.assemble();
     assert_eq!(c.len(), 3);
     assert_eq!(c[0], 2);
-    assert_eq!(c[1], 0x40);
+    assert_eq!(c[1], 0x20);
     assert_eq!(c[2], 0x60);
 }
